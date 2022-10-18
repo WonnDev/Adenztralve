dbf15ee8e1ea99a9b61533227c346c88a1fd2447
Wonn <wonndev@gmail.com>
2022-10-18 17:53:54 +0700

    'fixsomethings'

--- main.js
+++ main.js
@@ -1,11 +1,36 @@
 /**** slick slide ****/
-$('.autoplay').slick({
-    slidesToShow: 4,
+$('.slick-product').slick({
+    slidesToShow: 3,
     slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2500,
-    arrows: true,
+    // autoplay: true,
+    // autoplaySpeed: 7000,
+    arrows: false,
     dots: true,
+    responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 500,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
 });
 
 /***** hide navbar *****/
@@ -15,34 +40,9 @@ var prevScrollpos = window.pageYOffset;
             if (prevScrollpos > currentScrollPos) {
                 document.getElementById("nav").style.top = "0";
             } else {
-                document.getElementById("nav").style.top = "-36px";
+                document.getElementById("nav").style.top = "-115px";
             }
             prevScrollpos = currentScrollPos;
         }
 
 
-/** menu-hover **/
-$(".asian").mousemove(function (event) {
-    $(".showdown-content-menu-tour-plus-asian").css("display", "block");
-});
-$(".asian").mouseout(function (event) {
-    $(".showdown-content-menu-tour-plusasian").css("display", "none");
-});
-$(".america").mousemove(function (event) {
-    $(".showdown-content-menu-tour-plus-america").css("display", "block");
-});
-$(".america").mouseout(function (event) {
-    $(".showdown-content-menu-tour-plus-america").css("display", "none");
-});
-$(".europe").mousemove(function (event) {
-    $(".showdown-content-menu-tour-plus-europe").css("display", "block");
-});
-$(".europe").mouseout(function (event) {
-    $(".showdown-content-menu-tour-plus-europe").css("display", "none");
-});
-$(".australia").mousemove(function (event) {
-    $(".showdown-content-menu-tour-plus-australia").css("display", "block");
-});
-$(".australia").mouseout(function (event) {
-    $(".showdown-content-menu-tour-plus-australia").css("display", "none");
-});
